c2244afc54838992ddc213192d65cad6255a457d
Andrei Ulasau <andrei.ulasau@leverx.com>
2020-05-30 16:45:14 +0300

    fix: App.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './app';
+import { App } from './App';
 import { render } from 'react-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
